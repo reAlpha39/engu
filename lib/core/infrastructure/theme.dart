c84fd62fa751cb71081d3df64d7e3d2f7e5da34f
reAlpha39 <yunusw718@gmail.com>
2022-11-10 18:03:34 +0700

    add appbar theme foreground color

--- lib/core/infrastructure/theme.dart
+++ lib/core/infrastructure/theme.dart
@@ -14,6 +14,7 @@ ThemeData theme() {
     scaffoldBackgroundColor: CustomColor.primaryBgColor,
     appBarTheme: const AppBarTheme(
       backgroundColor: CustomColor.primaryBgColor,
+      foregroundColor: CustomColor.primaryColor,
       elevation: 0,
     ),
   );
