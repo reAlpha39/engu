2889641734d00cff633a2bc68d76c37a837d9313
Miftah-Maritah <muhammadmiftah998@gmail.com>
2022-11-10 23:29:42 +0700

    change color theme

--- lib/core/infrastructure/constant.dart
+++ lib/core/infrastructure/constant.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomColor {
-  static const Color primaryColor = Color(0xFFFFC006);
-  static const Color primaryBgColor = Color(0xFF323232);
-  static const Color secondaryBgColor = Color(0xFF222222);
+  static const Color primaryColor = Color(0xFF0066FF);
+  static const Color primaryBgColor = Color(0xFFF6F6F6);
+  static const Color secondaryBgColor = Color(0xFFFFFFFF);
 }
 
 const String baseApi = 'https://englishunires.com/api/';
