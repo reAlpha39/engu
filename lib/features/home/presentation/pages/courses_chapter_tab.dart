0aed26621a71e27707cbbc45c0adb0eb18c974a6
reAlpha39 <yunusw718@gmail.com>
2022-11-10 17:19:12 +0700

    seperate card courses widget

--- lib/features/home/presentation/pages/courses_chapter_tab.dart
+++ lib/features/home/presentation/pages/courses_chapter_tab.dart
@@ -1,9 +1,7 @@
-import 'package:engu/core/infrastructure/constant.dart';
-import 'package:engu/features/home/data/models/courses/courses.dart';
 import 'package:engu/features/home/presentation/blocs/list_course/list_course_cubit.dart';
+import 'package:engu/features/home/presentation/widgets/card_courses.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:velocity_x/velocity_x.dart';
 
 class CoursesChapterTab extends StatelessWidget {
   const CoursesChapterTab({super.key});
@@ -22,50 +20,5 @@ class CoursesChapterTab extends StatelessWidget {
         orElse: () => const SizedBox(),
       ),
     );
-  }
-}
-
-class CardCourse extends StatelessWidget {
-  final Courses data;
-  const CardCourse({super.key, required this.data});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: CustomColor.secondaryBgColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              data.name.text
-                  .color(CustomColor.primaryColor)
-                  .size(18)
-                  .bold
-                  .make(),
-              const SizedBox(height: 4),
-              data.description.text.white.make()
-            ],
-          ).expand(),
-          Container(
-            height: 50,
-            width: 50,
-            decoration: const BoxDecoration(
-              color: CustomColor.primaryColor,
-              shape: BoxShape.circle,
-            ),
-            child: const Icon(
-              Icons.play_arrow,
-              color: Colors.black,
-              size: 30,
-            ),
-          )
-        ],
-      ).p16(),
-    ).pOnly(bottom: 8);
   }
 }
---
+++ lib/features/home/presentation/widgets/card_courses.dart
@@ -0,0 +1,49 @@
+import 'package:engu/core/infrastructure/constant.dart';
+import 'package:engu/features/home/data/models/courses/courses.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class CardCourse extends StatelessWidget {
+  final Courses data;
+  const CardCourse({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: CustomColor.secondaryBgColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              data.name.text
+                  .color(CustomColor.primaryColor)
+                  .size(18)
+                  .bold
+                  .make(),
+              const SizedBox(height: 4),
+              data.description.text.white.make()
+            ],
+          ).expand(),
+          Container(
+            height: 50,
+            width: 50,
+            decoration: const BoxDecoration(
+              color: CustomColor.primaryColor,
+              shape: BoxShape.circle,
+            ),
+            child: const Icon(
+              Icons.play_arrow,
+              color: Colors.black,
+              size: 30,
+            ),
+          )
+        ],
+      ).p16(),
+    ).pOnly(bottom: 8);
+  }
+}
