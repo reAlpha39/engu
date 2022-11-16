109b9e176b422cd2bb7c9b3e9dccd26681070043
reAlpha39 <yunusw718@gmail.com>
2022-11-16 15:20:16 +0700

    add webview on quiz for english tab

--- lib/features/home/presentation/pages/quiz_for_english_tab.dart
+++ lib/features/home/presentation/pages/quiz_for_english_tab.dart
@@ -1,6 +1,8 @@
 import 'package:engu/core/infrastructure/constant.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:velocity_x/velocity_x.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class QuizForEnglishTab extends StatelessWidget {
   const QuizForEnglishTab({super.key});
@@ -8,13 +10,20 @@ class QuizForEnglishTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: double.infinity,
-      width: double.infinity,
+      height: MediaQuery.of(context).size.height,
       decoration: BoxDecoration(
         color: CustomColor.secondaryBgColor,
         borderRadius: BorderRadius.circular(10),
       ),
-      child: "QuizForEnglishTab".text.white.makeCentered(),
+      child: WebView(
+        initialUrl: 'https://englishunires.com/',
+        javascriptMode: JavascriptMode.unrestricted,
+        gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{}..add(
+            Factory<VerticalDragGestureRecognizer>(
+              () => VerticalDragGestureRecognizer(),
+            ),
+          ),
+      ),
     );
   }
 }
